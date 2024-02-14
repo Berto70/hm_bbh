d5169a333e9d2c491e2a93a33c79142fa081a048
Berto70 <gabriele.bertinelli@studenti.unipd.it>
2024-02-14 13:58:12 +0100

    added plot_ROC. not know if use it

--- hmbh.py
+++ hmbh.py
@@ -13,8 +13,8 @@ from typing import Optional, Tuple, Callable, Union, List
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV, learning_curve
 from sklearn.utils import resample
-from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay, roc_curve, auc
+from sklearn.preprocessing import MinMaxScaler, label_binarize
 
 from xgboost.sklearn import XGBClassifier
 
@@ -659,6 +659,64 @@ def plot_learning_curve(model: Union[RandomForestClassifier, XGBClassifier], X_t
     plt.grid()
 
     plt.show()
+
+
+def plot_ROC(model: Union[RandomForestClassifier, XGBClassifier], X_test: npt.NDArray, y_test: npt.NDArray, title: str):
+
+    n_classes = len(y_test.unique())
+
+    if n_classes == 2:
+    
+        # Predict probabilities of the positive class
+        y_score = model.predict_proba(X_test)[:, 1]
+
+        # Compute ROC curve and AUC
+        fpr, tpr, _ = roc_curve(y_test, y_score)
+        roc_auc = auc(fpr, tpr)
+
+        # Plot ROC curve
+        plt.figure()
+        plt.plot(fpr, tpr, color='tab:orange', lw=2, label='ROC curve positive class (AUC = %0.2f)' % roc_auc)
+        plt.plot([0, 1], [0, 1], color='gray', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC Curve for Binary-class (Test Set)\n' + title)
+        plt.legend(loc="lower right")
+        plt.legend(loc='lower right')
+        plt.show()
+    
+
+    else:
+        # Predict probabilities of each class for the testing set
+        y_score = model.predict_proba(X_test)
+        
+        # Binarize the labels
+        y_bin = label_binarize(y_test, classes=np.unique(y_test))
+
+        # Compute ROC curve and AUC for each class
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        for i in range(n_classes):
+            fpr[i], tpr[i], _ = roc_curve(y_bin[:, i], y_score[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+
+        # Plot ROC curve for each class
+        plt.figure()
+        colors = ['tab:blue', 'tab:green', 'tab:orange']
+        for i, color in zip(range(n_classes), colors):
+            plt.plot(fpr[i], tpr[i], color=color, lw=2, label='ROC curve (class {0}) (AUC = {1:0.2f})'.format(i, roc_auc[i]))
+
+        plt.plot([0, 1], [0, 1], color='gray', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC Curve for Multi-class (Test Set)')
+        plt.legend(loc='lower right')
+        plt.show()
 
 
 def shap_explainer(model: Union[RandomForestClassifier, XGBClassifier], data: Union[pd.DataFrame, npt.NDArray], n_sample: Union[int, bool]) -> List:
